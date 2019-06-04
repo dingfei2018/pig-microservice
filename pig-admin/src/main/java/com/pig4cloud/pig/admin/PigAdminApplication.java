ae0d54739d901f44a78037a5745569f52df3cd3a
dingfei <191154120@qq.com>
2019-06-04 18:53:56 +0800

    admin微服务

---
+++ pig-admin/src/main/java/com/pig4cloud/pig/admin/PigAdminApplication.java
@@ -0,0 +1,13 @@
+package com.pig4cloud.pig.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PigAdminApplication.class, args);
+    }
+
+}
